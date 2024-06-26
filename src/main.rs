6de95f0024da7e4bc0d6c68f186187782b9f1b19
DarkenedDragon <eridor314@gmail.com>
2024-06-25 20:51:41 -0400

    Added petgraph

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+use petgraph::graph::Graph;
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let batch_file_path = "test.bat";
+    let context_graph = Graph::<&str, ()>::default();
+    let mut operators_set = HashSet::<&str>::new();
+
+    // Add all the operators to the set
+    operators_set.insert("set");
+    operators_set.insert("if");
+    operators_set.insert("==");
+    operators_set.insert("=");
+    operators_set.insert("echo");
+    operators_set.insert("for");
+
+
+    // Go through the file word by word and build a context tree
+    let batch_file = fs::read_to_string(batch_file_path).expect(&format!("Cannot open {}", batch_file_path));
+    for word in batch_file.split_whitespace() {
+
+    }
 }
---
+++ test.bat
@@ -0,0 +1,11 @@
+@echo off
+
+REM This is a comment
+
+set VAR=1
+
+if ("VAR" == "1") (
+    echo VAR is 1
+) else (
+    echo VAR is not 1
+)
